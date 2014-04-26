76a70e8ba2c4d360e29c47847ac2bc9c74992941
Victor Mendiluce <vmendi@gmail.com>
2014-04-25 20:32:44 -0700

    Timeout 60s

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 5000,
 
-    browserNoActivityTimeout: 25000,
+    browserNoActivityTimeout: 60000,
 
     browsers: [ 'Chrome' ], 
     // browsers: [ 'Firefox', 'Chrome' ], 

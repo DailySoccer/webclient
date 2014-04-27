41e1fa8f04a6175f1deaf600a3ae5c63d12bce84
Victor Mendiluce <vmendi@gmail.com>
2014-04-26 18:54:34 -0700

    Running tests inside dart editor

--- test/webclient_test.dart
+++ test/webclient_test.dart
@@ -3,6 +3,9 @@ library webclient_test;
 import 'dart:math';
 import 'dart:async';
 import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart';
+import 'package:unittest/html_enhanced_config.dart';
+
 import 'package:angular/mock/module.dart';
 
 import "package:json_object/json_object.dart";
@@ -16,7 +19,6 @@ import 'package:webclient/services/contest_service.dart';
 
 import 'package:webclient/models/user.dart';
 import 'package:webclient/models/match_event.dart';
-import 'dart:html';
 import 'package:webclient/services/server_service.dart';
 
 part 'unit/profile_service_test.dart';
@@ -26,6 +28,14 @@ part 'unit/contest_service_test.dart';
 part 'unit/match_event_test.dart';
 
 main() {
+
+  // Cazamos la excepcion que salta cuando estamos corriendo bajo karma al intentar reconfigurar (puesto
+  // que karma configura primero)
+  try {
+    //useHtmlConfiguration();
+    useHtmlEnhancedConfiguration();
+  } catch (exc) {}
+
   testMatchDay();
   testMatchService();
   testMatchGroupService();

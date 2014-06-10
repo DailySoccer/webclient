1606226e6cf387249ddf5d2e796ce6c0ef6d57bd
Victor Mendiluce <vmendi@gmail.com>
2014-06-09 21:34:29 -0700

    Limpieza de active_contest_list

--- compile_less.sh
+++ compile_less.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # --compress --clean-css
-./node_modules/less/bin/lessc web/less/styles.less web/css/styles.css
+./node_modules/less/bin/lessc web/less/styles.less web/css/styles.css --compress --clean-css

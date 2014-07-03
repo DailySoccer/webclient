2aec900b6496a21fe20781f5fdaf5a0246980401
Ximo <xmalbors@hotmail.com>
2014-07-03 07:10:20 +0200

    Enter Contest: Botón de "Confirmar". Activo únicamente si todos los slots de jugadores están rellenos.

--- lib/services/enter_contest_service.dart
+++ lib/services/enter_contest_service.dart
@@ -2,6 +2,7 @@ library enter_contest_service;
 
 import 'package:angular/angular.dart';
 import 'package:webclient/models/field_pos.dart';
+import 'package:webclient/services/profile_service.dart';
 import 'package:webclient/services/contest_service.dart';
 import "package:webclient/models/soccer_player.dart";
 import "package:webclient/models/soccer_team.dart";
@@ -20,7 +21,7 @@ class EnterContestService {
   List<dynamic> availableSoccerPlayers = new List();
 
 
-  EnterContestService(this._contestService) {
+  EnterContestService(this._profileService, this._contestService) {
     // Creamos los slots iniciales, todos vacios
     FieldPos.LINEUP.forEach((pos) {
       lineupSlots.add(null);
@@ -119,24 +120,35 @@ class EnterContestService {
         _insertSoccerPlayer(matchEvent, matchEvent.soccerTeamB, player);
       }
     }
-    
-    /*
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("GOALKEEPER"), "fullName":"IKER CASILLAS", "matchEventName": "ATM - RMD", "remainingMatchTime": "70 MIN"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("DEFENSE"), "fullName":"DIEGO LOPEZ", "matchEventName": "RMD - VAL", "remainingMatchTime": "70 MIN"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("DEFENSE"), "fullName":"JESUS HERNANDEZ", "matchEventName": "RMD - ROS", "remainingMatchTime": "EMPIEZA 19:00"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("DEFENSE"), "fullName":"RAPHAEL VARANE", "matchEventName": "ATM - RMD", "remainingMatchTime": "70 MIN"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("DEFENSE"), "fullName":"PEPE", "matchEventName": "ATM - RMD", "remainingMatchTime": "70 MIN"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("MIDDLE"), "fullName":"SERGIO RAMOS", "matchEventName": "ATM - RMD", "remainingMatchTime": "70 MIN"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("MIDDLE"), "fullName":"NACHO FERNANDEZ", "matchEventName": "ATM - RMD", "remainingMatchTime": "EMPIEZA 19:00"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("MIDDLE"), "fullName":"FABIO COENTRAO", "matchEventName": "ATM - RMD", "remainingMatchTime": "70 MIN"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("MIDDLE"), "fullName":"MARCELO", "matchEventName": "ATM - RMD", "remainingMatchTime": "70 MIN"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("FORWARD"), "fullName":"ALVARO ARBELOA", "matchEventName": "ATM - RMD", "remainingMatchTime": "70 MIN"});
-    _allSoccerPlayers.add({"fieldPos":new FieldPos("FORWARD"), "fullName":"DANIEL CARVAJAL", "matchEventName": "ATM - RMD", "remainingMatchTime": "EMPIEZA 9:00"});
-    */
   }
 
+  void createFantasyTeam() {
+    // TODO: Se tendría que redireccionar a la pantalla de hacer "Login"?
+    if (!_profileService.isLoggedIn)
+      return;
+    
+    print("createFantasyTeam");
+
+    print("contest: " + contest.name);
+    print("user: " + _profileService.user.fullName);
+    
+    for (dynamic player in lineupSlots) {
+      print(player["fieldPos"].fieldPos + ": " + player["fullName"]);
+    }
+  }
+  
+  bool isFantasyTeamValid() {
+    for (dynamic player in lineupSlots) {
+      if (player == null) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
 
   var _allSoccerPlayers = new List();
   int _selectedLineupPosIndex = 0;
   ContestService _contestService;
+  ProfileService _profileService;
 }

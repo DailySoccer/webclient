c3914dd42d5dfd77f9211096d3382ca99ce25c9f
Ximo <xmalbors@hotmail.com>
2014-07-08 09:07:43 +0200

    MatchEvent, SoccerTeam y SoccerPlayer se referencian entre sí
    Dado que MatchEvent incrusta a sus elementos "hijos", es lógico que sus "hijos" conozcan a su "padre"
    
    MatchEvent incluye SoccerTeams
    SoccerTeam incluye SoccerPlayers y sabe en qué MatchEvent participa
    SoccerPlayer sabe en qué SoccerTeam juega

--- lib/models/soccer_team.dart
+++ lib/models/soccer_team.dart
@@ -2,6 +2,7 @@ library soccer_team;
 
 import "package:json_object/json_object.dart";
 import "package:webclient/models/soccer_player.dart";
+import "package:webclient/models/match_event.dart";
 
 class SoccerTeam {
   String templateSoccerTeamId;
@@ -9,13 +10,19 @@ class SoccerTeam {
   String shortName;
   List<SoccerPlayer> soccerPlayers = new List<SoccerPlayer>();
 
+  // Partido en el que juega
+  MatchEvent matchEvent;
+  
   SoccerTeam.fromJsonObject(JsonObject json) {
     templateSoccerTeamId = json.templateSoccerTeamId;
     name = json.name;
     shortName = json.shortName;
 
     for (var x in json.soccerPlayers) {
-      soccerPlayers.add(new SoccerPlayer.fromJsonObject(x));
+      SoccerPlayer soccerPlayer = new SoccerPlayer.fromJsonObject(x)
+        .. team = this;
+      
+      soccerPlayers.add(soccerPlayer);
     }
   }
 }

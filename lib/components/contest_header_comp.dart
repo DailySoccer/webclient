ce7df74891e0494c41ccb37543d6f0ee7d003949
Ximo <xmalbors@hotmail.com>
2014-07-09 16:40:37 +0200

    contest_header_comp: Muestra información básica sobre el contest

--- lib/components/contest_header_comp.dart
+++ lib/components/contest_header_comp.dart
@@ -2,6 +2,7 @@ library contest_header_comp;
 
 import 'package:angular/angular.dart';
 import 'package:webclient/services/screen_detector_service.dart';
+import 'package:webclient/controllers/live_contest_ctrl.dart';
 
 @Component(
     selector: 'contest-header', 
@@ -12,11 +13,11 @@ import 'package:webclient/services/screen_detector_service.dart';
 
 class ContestHeaderComp {
   var contestHeaderInfo = {
-    'description': '€11.250K Domingo Mongol',
+    'description': '',
     'startTime':'Empezó a las 20:45',
     'contestType': 'Liga Multijugador',
-    'contestantCount': '35 jugadores',    
-    'entryPrice': '€25',
+    'contestantCount': '',    
+    'entryPrice': '',
     'prize': '€65.000',
     'prizeType':'TODO PARA EL GANADOR'
   };
@@ -24,9 +25,24 @@ class ContestHeaderComp {
   String info;
   ScreenDetectorService scrDet;
   
-  ContestHeaderComp(Scope scope, this._router, this.scrDet) {
+  ContestHeaderComp(this._scope, this._router, this.scrDet, this._liveContestCtrl) {
     print('printing the contest info' + contestHeaderInfo.toString());
+    _scope.watch("updatedDate", (newValue, oldValue) {
+      _refreshHeader();
+    }, context: _liveContestCtrl);    
   }
 
+  void _refreshHeader() {
+    if (_liveContestCtrl.contest != null) {
+      contestHeaderInfo["description"] = "€${_liveContestCtrl.templateContest.salaryCap} ${_liveContestCtrl.contest.name}";
+      contestHeaderInfo["entryPrice"] = "€${_liveContestCtrl.templateContest.entryFee}";
+      
+      int numJugadores = _liveContestCtrl.contest.currentUserIds.length;
+      contestHeaderInfo["contestantCount"] = "$numJugadores" + ((numJugadores == 1) ? " jugador" : " jugadores");
+    }
+  }
+  
+  Scope _scope;
   Router _router;
+  LiveContestCtrl _liveContestCtrl;
 }

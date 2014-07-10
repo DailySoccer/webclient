c34715d94f37cfb93334cdbafc18a804615d3a6d
Ximo <xmalbors@hotmail.com>
2014-07-10 09:09:48 +0200

    profile_service: _tryProfileLoad intenta un "login" en background
    Dado que actualmente el simulador borra toda la información, la sesión almacenada en el cliente es inválida y requiere un login completo.
    Se realiza un "login" a escondidas (sin password) para facilitar las pruebas con el simulador.

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -62,6 +62,13 @@ class ProfileService {
 
     if (storedSessionToken != null && storedUser != null) {
       _setProfile(storedSessionToken, new User.fromJsonString(storedUser), false);
+      
+      // TODO: Hacemos un "login" a escondidas... (los "usuarios" pueden haber sido eliminados por el simulador)
+      login(user.email, "<no password>")
+        .then((jsonObject) {
+          print("profile load ok");
+        })
+        .catchError((error) => print("login error..."));
     }
   }
 

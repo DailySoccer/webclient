ae2e7b02ddc53ac9f98984ddc9f9bd30d6184853
SantiagoRevelo <sreveloc@gmail.com>
2014-07-18 15:32:10 +0200

    arreglo llamada a ventana modal desde Dart

--- lib/components/lobby_comp.dart
+++ lib/components/lobby_comp.dart
@@ -2,6 +2,7 @@ library active_contest_list_comp;
 
 import 'dart:html';
 import 'dart:async';
+import 'dart:js' as js;
 import 'package:angular/angular.dart';
 import 'package:webclient/services/active_contests_service.dart';
 import 'package:webclient/models/contest.dart';
@@ -27,7 +28,13 @@ class LobbyComp implements DetachAware {
     selectedContest = contest;
 
     // Esto soluciona el bug por el que no se muestra la ventana modal en Firefox;
-    querySelector('#infoContestModal').style.display = "block";
+    var modal = querySelector('#infoContestModal');
+    modal.style.display = "block";
+
+    // Con esto llamamos a funciones de jQuery
+    js.context.callMethod(r'$', ['#infoContestModal'])
+      .callMethod('modal', [new js.JsObject.jsify({'show': 'true'})]);
+
   }
 
   void onActionClick(Contest contest) {

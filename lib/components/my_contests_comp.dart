bec0ef81c18473b83d0dbd8f1e4116c2d8cf8c44
Victor Mendiluce <vmendi@gmail.com>
2014-07-17 21:01:42 -0700

    Flujo corregido, quitado el hack del live_contest_ctrl

--- lib/components/my_contests_comp.dart
+++ lib/components/my_contests_comp.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:webclient/services/my_contests_service.dart';
 import 'package:webclient/services/flash_messages_service.dart';
+import 'package:webclient/models/contest.dart';
 
 
 
@@ -16,10 +17,30 @@ class MyContestsComp {
 
   MyContestsService myContestsService;
 
-  MyContestsComp(this.myContestsService, this._flashMessage) {
+  MyContestsComp(this.myContestsService, this._router, this._flashMessage) {
     myContestsService.refreshMyContests()
       .catchError((error) => _flashMessage.error("$error", context: FlashMessagesService.CONTEXT_VIEW));
   }
 
+  void onWaitingRowClick(Contest contest) {
+  }
+
+  void onWaitingActionClick(Contest contest) {
+  }
+
+  void onLiveRowClick(Contest contest) {
+  }
+
+  void onLiveActionClick(Contest contest) {
+    _router.go('live_contest', {"contestId" : contest.contestId});
+  }
+
+  void onHistoryRowClick(Contest contest) {
+  }
+
+  void onHistoryActionClick(Contest contest) {
+  }
+
+  Router _router;
   FlashMessagesService _flashMessage;
 }

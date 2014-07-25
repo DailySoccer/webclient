f15034ac8f9e157ad3c08d4a5a530746d30d2542
Fede Mon <gnu.fede@gmail.com>
2014-07-25 11:06:42 +0200

    Added drone launch script

---
+++ drone.sh
@@ -0,0 +1,8 @@
+# sudo start xvfb
+sudo npm install
+node --version
+pub install
+pub build
+# # /usr/bin/firefox &
+# # /usr/bin/google-chrome &
+# # ./node_modules/karma/bin/karma start karma.conf.js --singleRun --browsers PhantomJS

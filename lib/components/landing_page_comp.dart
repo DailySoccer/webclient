3fbf35fb971b84b7d00d2063a372b58453112d1d
SantiagoRevelo <sreveloc@gmail.com>
2014-07-29 15:05:40 +0200

    LandingPage rehaciendose 2

--- lib/components/landing_page_comp.dart
+++ lib/components/landing_page_comp.dart
@@ -35,7 +35,7 @@ class LandingPageComp implements ShadowRootAware, DetachAware {
     mainWrapper.classes.clear();
     mainWrapper.classes.add('landing-wrapper');
     containerForContent.classes.clear();
-    containerForContent.style.backgroundColor = "tranparent";
+    //containerForContent.style.backgroundColor = "tranparent";
     bodyObj.classes.add('fondo-negro');
   }
 
@@ -43,6 +43,7 @@ class LandingPageComp implements ShadowRootAware, DetachAware {
     mainWrapper.classes.clear();
     mainWrapper.classes.add('wrapper-content-container');
     containerForContent.classes.add('main-content-container');
+    //containerForContent.style.backgroundColor = "#FFFFFF";
     bodyObj.classes.remove('fondo-negro');
   }
 

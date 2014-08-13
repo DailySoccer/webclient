cab3233f372d591c8d94d5aca5746609c297d8bd
Ximo <xmalbors@hotmail.com>
2014-08-13 13:50:09 +0200

    flash_messages sin tiempo

--- lib/services/flash_messages_service.dart
+++ lib/services/flash_messages_service.dart
@@ -7,15 +7,11 @@ class FlashMessage {
   String type;
   String text;
 
-  DateTime createdAt;
-
   FlashMessage(this.context, this.type, this.text);
 }
 
 @Injectable()
 class FlashMessagesService {
-  static const MESSAGE_DURATION = 10;
-
   static const CONTEXT_GLOBAL = "GLOBAL";
   static const CONTEXT_VIEW   = "VIEW";
 
@@ -24,27 +20,10 @@ class FlashMessagesService {
   static const TYPE_WARNING = "warning";
   static const TYPE_ERROR   = "danger";
 
-  List<FlashMessage> get messages {
-    DateTime now = new DateTime.now();
-
-    // Cada x tiempo...
-    if (now.difference(_filteredLastTime).inSeconds > 1) {
-      _filteredLastTime = now;
-
-      // Comprobamos si existe algún mensaje antiguo...
-      // Nota: Lo hacemos en dos pasos para que, de no ser necesario, no se considere que ha habido algún cambio
-      bool isOldMsg(FlashMessage m) => (now.difference(m.createdAt).inSeconds >= MESSAGE_DURATION);
-      if (_messages.any( (m) => isOldMsg(m) )) {
-        _messages = _messages.where( (m) => !isOldMsg(m) ).toList();
-      }
-    }
-
-    return _messages;
-  }
-
+  List<FlashMessage> messages = new List<FlashMessage>();
 
   clearContext(String context) {
-    _messages = _messages.where( (m) => m.context != context ).toList();
+    messages = messages.where( (m) => m.context != context ).toList();
   }
 
   warning(String text, {context: CONTEXT_GLOBAL})  => _addMessage(new FlashMessage(context, TYPE_WARNING, text));
@@ -53,10 +32,6 @@ class FlashMessagesService {
   error(String text, {context: CONTEXT_GLOBAL})    => _addMessage(new FlashMessage(context, TYPE_ERROR, text));
 
   _addMessage(FlashMessage message) {
-    message.createdAt = new DateTime.now();
-    _messages.add(message);
+    messages.add(message);
   }
-
-  DateTime _filteredLastTime = new DateTime.now();
-  var _messages = new List<FlashMessage>();
 }

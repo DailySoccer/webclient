11875fb21796b027f33d11641fda7f0295b0bc9f
Victor Mendiluce <vmendi@gmail.com>
2014-08-21 03:05:54 -0700

    Limit to dot.

--- lib/utils/limit_to_dot.dart
+++ lib/utils/limit_to_dot.dart
@@ -1,33 +1,19 @@
 library limit_to_dot;
 
-import 'package:di/di.dart';
 import 'package:angular/core/annotation.dart';
 
 @Formatter(name:'limitToDot')
 class LimitToDot implements Function {
-  Injector _injector;
-  bool _dotted;
 
-  LimitToDot(this._injector);
+  String call(String items, int limit) {
 
-  dynamic call(String items, [int limit]) {
-    if (items == null) 
-      return null;
-    if (limit == null) 
-      limit = -1;
-   
-    int i = 0, j = items.length;
-    if (limit > -1) {
-      _dotted = (j > limit) ? true : false;
-      j = (limit > j) ? j : limit; 
-    } else {
-      _dotted = false;
+    int j = items.length;
+
+    if (j > limit) {
+      return items.substring(0, limit) + '…';
     }
-    
-    if (_dotted) {
-      return items.substring(i, j) + '…';
-    } else {
-      return items.substring(i, j);
+    else {
+      return items.substring(0, j);
     }
   }
 }

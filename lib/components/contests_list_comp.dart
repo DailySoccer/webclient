5add2f3e03abe8dd68b4907b2aad37382f19f8ad
SantiagoRevelo <sreveloc@gmail.com>
2014-08-27 11:52:58 +0200

    Terminada la paginación de las listas de concursos

--- lib/components/contests_list_comp.dart
+++ lib/components/contests_list_comp.dart
@@ -233,32 +233,23 @@ class ContestsListComp {
   /*************************************/
   /*****  Paginador functionality  *****/
   /*************************************/
-
-
-
-  var _meta;
-
-  Map _defaults = {
-    "itemContainerId"       : ".content",
-    "itemPerPage"           : 10,
-    "navPanelId0"           : ".page-navigation",
-    "navInfoId"             : ".info-text",
-    "numPageLinksToDisplay" : 20,
-    "startPage"             : 0,
-    "wrapAround"            : false,
+  Map options = {
+    "itemsPerPage"          : 10,
+    "navPanelId"            : "#paginatorBox_",
+    "linkButtonId"          : "linkButton",
+    "pageLinksIdentifier"   : "page-link",
+    "numPageLinksToDisplay" : 3,
     "navLabelFirst"         : "&laquo;",
     "navLabelPrev"          : "&lt;",
     "navLabelNext"          : "&gt",
     "navLabelLast"          : "&raquo;",
     "navOrder"              : ["first", "prev", "num", "next", "last"],
-    "navLabelInfo"          : "Showing [0]-[1] of {2} results",
     "showFirstLast"         : true,
-    "abortOnSmallLists"     : true,
     "stateActive"           : "active",
     "stateDisabled"         : "disabled"
   };
+
   int _currentPage = 0;
-  int _itemsPerPage = 5;
   int _totalPages = 0;
 
   List<Contest> currentPageList = [];
@@ -272,38 +263,196 @@ class ContestsListComp {
       print('-CONTEST_LIST-: El nombre de esta lista de concursos es null');
     }
     // Capturamos el elementos qe será el padre del paginador.
-    String paginatorContainerId = "paginatorBox" + listName;
-    _paginatorContainer = querySelector('#'+paginatorContainerId);
+    options["navPanelId"] = "#paginatorBox_" + listName;
+    _paginatorContainer = querySelector(options["navPanelId"]);
 
     if(contestsListFiltered != null){
-    //  Determinamos que elementos se mostrarán en la first page
-    //Calculamos la páginas que habrá en total y determinamos si el paginador estará disponible o no.
-    _totalPages = (contestsListFiltered.length /_itemsPerPage).ceil();
-    if(_totalPages > 1)
-      paginatorAvailable = true;
 
-    ///////
-    if(_paginatorContainer != null) {
-      _paginatorContainer.text = "El paginador tendrá ${_totalPages} páginas";
-      _paginatorContainer.style.width = "150px";
+      //Calculamos la páginas que habrá en total y determinamos si el paginador estará disponible o no.
+      _totalPages = (contestsListFiltered.length /options["itemsPerPage"]).ceil();
+
+      paginatorAvailable = _totalPages > 1 ? true : false;
+
+      if(paginatorAvailable){
+        generatePaginatorButtons();
+        // Determinamos que elementos se mostrarán en la pagina actual
+        int rangeStart = _currentPage * options["itemsPerPage"];
+        int rangeEnd =  (rangeStart + options["itemsPerPage"] < contestsListFiltered.length) ? rangeStart + options["itemsPerPage"] : contestsListFiltered.length;
+        currentPageList = contestsListFiltered.getRange(rangeStart, rangeEnd).toList();
+      }
+      else//Si solo elementos para rellenar una página mostramos la lista talcual
+      {
+        if(_paginatorContainer != null)
+          _paginatorContainer.innerHtml="";
+
+        currentPageList = contestsListFiltered;
+      }
     }
-    /////
-
-    if(paginatorAvailable){
-      currentPageList = contestsListFiltered.getRange(_currentPage * _itemsPerPage , _itemsPerPage).toList();
-    }
-    else //Si solo elementos para rellenar una página mostramos la lista talcual
-      currentPageList = contestsListFiltered;
-    }
-
-
-
   }
 
+  void generatePaginatorButtons() {
+    // Limpiamos el paginador
+    _paginatorContainer.innerHtml="";
+
+    // Elementos fijos de la lista
+    UListElement ul = new UListElement();
+    ul.classes.add("pagination");
+
+    // El botón de ir a la "Primera"
+    LIElement first = createPageOption(options["navLabelFirst"], null, ["to-first-page"] );
+
+   // El botón de ir a la "Anterior"
+    LIElement prev = createPageOption(options["navLabelPrev"], null, ["to-prev-page"] );
+
+    // El botón de ir a la "Siguiente"
+    LIElement next =  createPageOption(options["navLabelNext"], null, ["to-next-page"] );
+
+    // El botón de ir a la "Última"
+    LIElement last = createPageOption(options["navLabelLast"], null, ["to-last-page"] );
+
+    //Además creamos el botón de "..." que indica que hay mas botones antes
+    LIElement less = createPageOption("...", null, ["ellipsis", "less"] );
+    //Además creamos el botón de "..." que indica que hay mas botones después
+    LIElement more = createPageOption("...", null, ["ellipsis", "more"] );
+
+    // Generamos la lista de elementos que tendrá el paginador
+    for (int i=0; i < options["navOrder"].length; i++) {
+      switch(options["navOrder"][i]) {
+        case "first":
+          if(options["showFirstLast"])
+            ul.children.add(first);
+        break;
+        case "prev":
+          ul.children.add(prev);
+        break;
+        case "num":
+         ul.children.add(less);
+         int currentLink = 0;
+         while (_totalPages > currentLink) {
+           // El primer elemento se linka con la página 0 aunque su label sea 1;
+           ul.children.add(createPageOption("${currentLink + 1}", currentLink, [options["pageLinksIdentifier"]]));
+          currentLink++;
+         }
+         ul.children.add(more);
+        break;
+        case "next":
+          ul.children.add(next);
+        break;
+        case "last":
+          if(options["showFirstLast"])
+            ul.children.add(last);
+        break;
+      }
+    };
 
 
+    // Los botones de puntos suspensivos los ocultamos
+   var ellipsisButtons  = ul.children.where( (Element element) => element.classes.contains('ellipsis')).toList();
+   ellipsisButtons.forEach( (LIElement element) => element.style.display = 'none');
 
+   // Llista inks de enlaces a páginas
+   var links = ul.children.where( (element) => element.classes.contains(options["pageLinksIdentifier"])).toList();
+    //Activamos la página actual
+   links[_currentPage].classes.add(options["stateActive"]);
 
+   // Ocultamos todos los links de páginas...
+   links.forEach((LIElement element) => element.style.display = 'none');
+   // ... Y mostramos X elementos según el parametro 'numPageLinkdToDisplay' definido en las opciones
+   List<dynamic> visibles;
+   if(links.length > options["numPageLinksToDisplay"]) {
+      int rangeStarts = calculateRangeStart(_currentPage, options["numPageLinksToDisplay"], 0, links.length);
+      int rangeEnds = rangeStarts + options["numPageLinksToDisplay"];
+      visibles = links.getRange(rangeStarts, rangeEnds).toList();
+   } else {
+     visibles = links;
+   }
+   visibles.forEach((LIElement element) => element.style.display = '');
 
+   //TODO: Toggle less y more buttons e ya!;
+   toggleEllipsis(visibles, ul.children);
 
+   //Añadimos los elementos al contenedor padre
+   _paginatorContainer.children.add(ul);
+  }
+
+  // Crea un element <li> con un <a> en su interior que linkará con la página correspondiente
+  // de la lista, y que añadiremos al paginador;
+  LIElement createPageOption(String label, int pageNum, List<String> cssClasses) {
+    AnchorElement a = new AnchorElement()
+      ..innerHtml =label;
+
+    //if(pageNum != null) // Los botones que llevan "..." no llevan, por eso se especifica pageNum = -1
+    a.on['click'].listen((event) => goToPage( a, pageNum));
+
+    if(pageNum == 0)
+      cssClasses.add('first-page');
+    if(pageNum == _totalPages - 1)
+      cssClasses.add('last-page');
+
+    String myName = "${options["linkButtonId"]}_${listName}_";
+    myName += pageNum == null? cssClasses.join("-") : pageNum.toString();
+    print("añadido botón con id ${myName}");
+    LIElement li = new LIElement()
+      ..children.add(a)
+      ..classes.addAll(cssClasses)
+      ..id = myName;
+    return li;
+  }
+
+  // Calcula el valor inicial de un rango
+  int calculateRangeStart(int current, int size, int minLmit, int maxLimit) {
+
+    int retorno;
+    int average = (size / 2).ceil()-1;
+    // El limite inferior es posible es minLmit
+    retorno  = current - average < minLmit ? minLmit : current - average;
+
+    if(retorno > maxLimit - size)
+        retorno = maxLimit- size;
+    return retorno;
+  }
+
+  void toggleEllipsis(List<dynamic> currentButtonsShowed, List<dynamic> buttonList) {
+    List<Element> first = currentButtonsShowed.where((Element element) => element.classes.contains("first-page")).toList();
+    List<Element> last = currentButtonsShowed.where((Element element) => element.classes.contains("last-page")).toList();
+
+    var lessButton = buttonList.where((Element element) => element.classes.contains("less")).toList().first;
+    lessButton.style.display = first.isEmpty ? '' : 'none';
+
+    var moreButton = buttonList.where((Element element) => element.classes.contains("more")).toList().first;
+    moreButton.style.display = last.isEmpty ? '' : 'none';
+  }
+
+  void goToPage(Element target, int pageNum) {
+    if(pageNum == null)
+    {
+      //print("Han pulsado en el botón ${target.parent.classes}");
+      switch(target.parent.classes.first)
+      {
+        case "to-first-page":
+          _currentPage = 0;
+        break;
+        case "to-prev-page":
+          _currentPage--;
+        break;
+        case "to-next-page":
+          _currentPage++;
+        break;
+        case "to-last-page":
+          _currentPage = _totalPages - 1;
+        break;
+      }
+    }
+    else
+      _currentPage = pageNum;
+
+    if(_currentPage < 0)
+      _currentPage = 0;
+    if(_currentPage > _totalPages -1)
+      _currentPage = _totalPages -1;
+
+    print("Mostrando página ${_currentPage}");
+
+    createPaginator();
+  }
 }

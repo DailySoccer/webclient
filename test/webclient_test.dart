9763388858462a961522e7030c7838777437e801
Victor Mendiluce <vmendi@gmail.com>
2014-08-28 00:41:07 -0700

    Una desconexion que no provoca warnings en pub serve.

--- test/webclient_test.dart
+++ test/webclient_test.dart
@@ -1,11 +1,30 @@
 library webclient_test;
 
+// 27/08/2014: Comentamos el codigo real de test puesto que se habia quedado obsoleto y no lo mantenemos,
+//             dejamos unicamente el esqueleto del framework de testeo.
+
+
+/*
+import 'dart:html';
+import 'dart:math';
+import 'dart:async';
 import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart';
 import 'package:unittest/html_enhanced_config.dart';
-// import 'package:unittest/html_config.dart';
+
+import "package:json_object/json_object.dart";
+
+import 'package:webclient/mock/mock_server.dart';
+
+import 'package:webclient/services/profile_service.dart';
+import 'package:webclient/services/active_contests_service.dart';
+
+import 'package:webclient/models/match_event.dart';
+import 'package:webclient/models/contest.dart';
 
 part 'unit/profile_service_test.dart';
 part 'unit/contest_service_test.dart';
+
 
 main() {
 
@@ -16,12 +35,9 @@ main() {
     useHtmlEnhancedConfiguration();
   } catch (exc) {}
 
-  // Podemos filtrar ciertos grupos
-  // filterTests("valid");
-
-  // 27/08/2014: Comentamos el codigo real de test puesto que se habia quedado obsoleto y no lo mantenemos,
-  //             dejamos unicamente el esqueleto del framework de testeo.
   testContestService();
   testProfileService();
 
+  // filterTests("valid");
 }
+*/

dd01a485436c1e602eb2a1c588cd1dc0ae3b9907
SantiagoRevelo <sreveloc@gmail.com>
2014-09-23 11:03:09 +0200

    Added bootstrap-switch

--- lib/components/edit_personal_data_comp.dart
+++ lib/components/edit_personal_data_comp.dart
@@ -2,15 +2,11 @@ library edit_personal_data_comp;
 
 import 'package:angular/angular.dart';
 import 'package:webclient/services/profile_service.dart';
+import 'package:webclient/utils/js_utils.dart';
 
-@Component(
-    selector: 'edit-personal-data',
-    templateUrl: 'packages/webclient/components/edit_personal_data_comp.html',
-    publishAs: 'comp',
-    useShadowDom: false
-)
+@Component(selector: 'edit-personal-data', templateUrl: 'packages/webclient/components/edit_personal_data_comp.html', publishAs: 'comp', useShadowDom: false)
 
-class EditPersonalDataComp {
+class EditPersonalDataComp implements ShadowRootAware {
 
   String password;
   String repeatPassword;
@@ -18,9 +14,81 @@ class EditPersonalDataComp {
   String region;
   String city;
 
+  @NgTwoWay("is-pop-up")
+   bool get isPopUp => _popUpStyle;
+   void set isPopUp (bool value){
+     _popUpStyle = value;
+   }
+
+  bool get acceptNewsletter => _acceptNewsletter;
+  void set acceptNewsletter(bool value) {
+    _acceptNewsletter = value;
+    print('-EDIT_PERSONAL_DATA-: acceptNewsletter = ${_acceptNewsletter}');
+  }
+
+  bool get acceptGameAlerts => _acceptGameAlerts;
+  void set acceptGameAlerts(bool value) {
+    _acceptGameAlerts = value;
+    print('-EDIT_PERSONAL_DATA-: acceptGameAlerts = ${_acceptGameAlerts}');
+  }
+
+  bool get acceptSoccerPlayerAlerts => _acceptSoccerPlayerAlerts;
+  void set acceptSoccerPlayerAlerts(bool value) {
+    _acceptSoccerPlayerAlerts = value;
+    print('-EDIT_PERSONAL_DATA-: acceptSoccerPlayerAlerts = ${_acceptSoccerPlayerAlerts}');
+  }
+
   dynamic get userData => _profileManager.user;
 
   EditPersonalDataComp(this._profileManager);
 
+  @override
+  void onShadowRoot(root) {
+    //switch NEWSLETTER/OFERTAS ESPECIALES
+    JsUtils.runJavascript("[name='switchNewsletter']", 'bootstrapSwitch', {
+                                                                                    'size': 'mini',
+                                                                                    'state': acceptNewsletter,
+                                                                                    'disabled'      : 'true',
+                                                                                    'onColor': 'primary',
+                                                                                    'offColor': 'default',
+                                                                                    'onSwitchChange': onNewsLetterSwitchChange
+                                                                           });
+    //switch NOTIFICACIONES DE JUEGO
+    JsUtils.runJavascript("[name='switchGameAlerts']", 'bootstrapSwitch', {
+                                                                                    'size': 'mini',
+                                                                                    'state': _acceptGameAlerts,
+                                                                                    'disabled'      : 'true',
+                                                                                    'onColor': 'primary',
+                                                                                    'offColor': 'default',
+                                                                                    'onSwitchChange': onGameAlertsSwitchChange
+                                                                          });
+    //switch NOTIFICACIONES DE TUS FICHAJES
+    JsUtils.runJavascript("[name='switchsoccerPlayerAlerts']", 'bootstrapSwitch', {
+                                                                                    'size': 'mini',
+                                                                                    'state': _acceptSoccerPlayerAlerts,
+                                                                                    'disabled'      : 'true',
+                                                                                    'onColor': 'primary',
+                                                                                    'offColor': 'default',
+                                                                                    'onSwitchChange': onSoccerPlayerAlertsSwitchChange
+                                                                                  });
+  }
+
+  void onNewsLetterSwitchChange(event, state) {
+    acceptNewsletter = state;
+  }
+
+  void onGameAlertsSwitchChange(event, state) {
+    acceptGameAlerts = state;
+  }
+
+  void onSoccerPlayerAlertsSwitchChange(event, state) {
+    acceptSoccerPlayerAlerts = state;
+  }
+
   ProfileService _profileManager;
+  bool _acceptNewsletter;
+  bool _acceptGameAlerts;
+  bool _acceptSoccerPlayerAlerts;
+
+  bool _popUpStyle;
 }

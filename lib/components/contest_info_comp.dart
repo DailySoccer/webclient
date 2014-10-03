99fc3e5707f8b1a1a36c043d3289d3fec5e7ef89
SantiagoRevelo <sreveloc@gmail.com>
2014-10-03 17:00:35 +0200

    .

--- lib/components/contest_info_comp.dart
+++ lib/components/contest_info_comp.dart
@@ -41,14 +41,14 @@ class ContestInfoComp implements ShadowRootAware {
   ContestInfoComp(Scope scope, this._router, this._contestService, this._flashMessage) {
 
     currentInfoData = {  /*  hay que utilizar esta variable para meter los datos de este componente  */
-      'description'     : '<description>',
-      'name'            : '<name>',
-      'entry'           : '<entry>',
-      'prize'           : '<prize>',
+      'description'     : 'cargando...',
+      'name'            : 'cargando...',
+      'entry'           : 'cargando...',
+      'prize'           : 'cargando...',
       'rules'           : 'Elige un equipo de 11 jugadores a partir de los siguientes partidos',         // 'Elige un equipo de 11 jugadores a partir de los siguientes partidos',
-      'startDateTime'   : '<startDateTime>', // 'COMIENZA EL DOM. 15/05 19:00',
+      'startDateTime'   : 'cargando...', // 'COMIENZA EL DOM. 15/05 19:00',
       'matchesInvolved' : null,
-      'legals'          : '<legals>',
+      'legals'          : 'cargando...',
       'contestants'     : contestants,
       'prizes'          : []
     };

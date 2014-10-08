a200bf97257cefb40b31e658c99df3b8dc7c28d7
Fede Mon <gnu.fede@gmail.com>
2014-10-08 15:08:05 +0200

    Build accepts debug or release parameters

--- build_rsync.sh
+++ build_rsync.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+mode="release"
+
+if [[ $1 != "" ]]
+    then
+        mode=$1
+fi
+
 # mode puede ser debug|relesae
-pub build --mode=release
+pub build --mode=$mode
 rsync -r  -v --copy-unsafe-links build/web/. ../backend/public/

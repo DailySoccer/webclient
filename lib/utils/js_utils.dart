3afabf25ec6ea5853fcbf2003d22ddfd46d47cc0
Victor Mendiluce <vmendi@gmail.com>
2014-10-10 03:53:01 -0700

    Espacios verticales.

--- lib/utils/js_utils.dart
+++ lib/utils/js_utils.dart
@@ -5,10 +5,10 @@ import 'dart:js' as js;
 class JsUtils {
   // Funcion para ejecutar los JavaScripts que necesitamos en esta página
   static dynamic runJavascript(String selector, String method, dynamic params, [bool option]) {
+
     // Si tenemos el tercer parametro
     if (option != null) {
       return js.context.callMethod(r'$', [selector]).callMethod(method, [new js.JsObject.jsify(params), true]);
-
     }
 
     if (params == null) { //javascript / jquery sin parametros
@@ -18,6 +18,7 @@ class JsUtils {
       if (params.toString().contains('[') || params.toString().contains('{')  ) // Si tienen un objeto iterable
         return js.context.callMethod(r'$', [selector]).callMethod(method, [new js.JsObject.jsify(params)]);
     } //javascript / jquery con parametro de valor simple
+
     return js.context.callMethod(r'$', [selector]).callMethod(method, [params]);
   }
 }

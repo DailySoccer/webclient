af00590414ddd2a88e966cc78fa12c8eede694ae
Fede Mon <gnu.fede@gmail.com>
2014-10-16 12:03:18 +0200

    Fixes

--- build_rsync.sh
+++ build_rsync.sh
@@ -7,7 +7,7 @@ if [[ $1 != "" ]]
         mode=$1
 fi
 
-if [[ $mode=="release" ]]
+if [[ "$mode"=="release" ]]
     then
     # Aplicamos cambios de release
     client_branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
@@ -23,10 +23,11 @@ fi
 ./compile_less.sh
 
 # mode puede ser debug|relesae
+echo $mode
 pub build --mode=$mode
 rsync -r  -v --copy-unsafe-links build/web/. ../backend/public/
 
-if [[ $mode=="release" ]]
+if [[ "$mode"=="release" ]]
     then
         # Volvemos a la rama original
         git checkout $client_branch_name

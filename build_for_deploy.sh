b16b175220b9ec5ac8a58e6c034a964c3b56bf11
vmendi <vmendi@gmail.com>
2014-10-23 23:35:04 -0700

    Hay que borrar template_gen.dart too.

--- build_for_deploy.sh
+++ build_for_deploy.sh
@@ -22,6 +22,7 @@ dart cache_gen.dart
 pub build --mode=$mode
 rsync -r  -v --copy-unsafe-links build/web/. ../backend/public/
 
-# Revertimos los cambios hechos al index.html
+# Revertimos los cambios hechos a distintos ficheros
 git checkout -- web/index.html
 git checkout -- web/css/styles.css
+git checkout -- lib/template_cache.dart

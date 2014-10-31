2a8e86b875e5218f96b7f481d688c3db30784b83
vmendi <vmendi@gmail.com>
2014-10-30 19:24:30 -0700

    Cambio del orden del TemplateCache

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,7 @@
-import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
-import 'package:angular/core_dom/static_keys.dart';
 
 import 'package:webclient/webclient.dart';
 import 'package:webclient/logger_exception_handler.dart';
-import 'package:webclient/template_cache.dart';
-import 'package:webclient/utils/host_server.dart';
 import 'package:webclient/utils/game_metrics.dart';
 
 void main() {
@@ -14,22 +10,9 @@ void main() {
     LoggerExceptionHandler.setUpLogger();
     GameMetrics.initMixpanel();
 
-    var app = applicationFactory().addModule(new WebClientApp());
-    var injector = app.run();
-
-    // El cache de templates solo lo primeamos en produccion, pq es donde tenemos garantizado que se habra
-    // hecho una build y por lo tanto el fichero lib/template_cache.dart tendra todos los contenidos frescos
-    if (HostServer.isProd) {
-      setUpCache(injector);
-    }
+    applicationFactory().addModule(new WebClientApp()).run();
   }
   catch (exc, stackTrace) {
     LoggerExceptionHandler.logExceptionToServer(exc, stackTrace);
   }
 }
-
-void setUpCache(injector) {
-  TemplateCache cache = injector.getByKey(TEMPLATE_CACHE_KEY);
-  primeTemplateCache(cache);
-}
-

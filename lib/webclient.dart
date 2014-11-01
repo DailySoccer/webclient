758409ad4faa78f9c736d662ded07190f56e95fd
vmendi <vmendi@gmail.com>
2014-11-01 12:13:43 -0700

    Limpieza

--- lib/webclient.dart
+++ lib/webclient.dart
@@ -3,6 +3,10 @@ library webclient;
 import 'package:angular/angular.dart';
 import 'package:angular/routing/module.dart';
 import 'package:webclient/webclient_router.dart';
+import 'package:webclient/logger_exception_handler.dart';
+
+import 'package:webclient/services/server_service.dart';
+import 'package:webclient/services/screen_detector_service.dart';
 
 import 'package:webclient/services/refresh_timers_service.dart';
 import 'package:webclient/services/loading_service.dart';
@@ -11,28 +15,19 @@ import 'package:webclient/services/profile_service.dart';
 import 'package:webclient/services/active_contests_service.dart';
 import 'package:webclient/services/my_contests_service.dart';
 import 'package:webclient/services/soccer_player_service.dart';
-import 'package:webclient/services/server_service.dart';
 import 'package:webclient/services/flash_messages_service.dart';
-import 'package:webclient/services/screen_detector_service.dart';
 import 'package:webclient/services/scoring_rules_service.dart';
 
-import 'package:webclient/logger_exception_handler.dart';
+import 'package:webclient/components/landing_page_comp.dart';
 
 import 'package:webclient/utils/limit_to_dot.dart';
 import 'package:webclient/utils/form-autofill-fix.dart';
 import 'package:webclient/utils/element-autofocus.dart';
 
-import 'package:webclient/components/flash_messages_comp.dart';
-import 'package:webclient/components/landing_page_comp.dart';
 import 'package:webclient/components/navigation/main_menu_slide_comp.dart';
 import 'package:webclient/components/navigation/footer_comp.dart';
 import 'package:webclient/components/global_connection_comp.dart';
-
-import 'package:webclient/components/contest_info_comp.dart';
-import 'package:webclient/components/scoring_rules_comp.dart';
-import 'package:webclient/components/contest_header_comp.dart';
-
-import 'package:webclient/components/my_contests_comp.dart';
+import 'package:webclient/components/flash_messages_comp.dart';
 
 import 'package:webclient/components/paginator_comp.dart';
 import 'package:webclient/components/contest_filters_comp.dart';
@@ -40,17 +35,22 @@ import 'package:webclient/components/lobby_comp.dart';
 import 'package:webclient/components/promos_comp.dart';
 import 'package:webclient/components/contests_list_comp.dart';
 
+import 'package:webclient/components/contest_info_comp.dart';
+import 'package:webclient/components/scoring_rules_comp.dart';
+import 'package:webclient/components/contest_header_comp.dart';
+
+import 'package:webclient/components/account/login_comp.dart';
+import 'package:webclient/components/account/join_comp.dart';
 import 'package:webclient/components/account/user_profile_comp.dart';
 import 'package:webclient/components/account/edit_personal_data_comp.dart';
 import 'package:webclient/components/account/remember_password_comp.dart';
-import 'package:webclient/components/account/login_comp.dart';
-import 'package:webclient/components/account/join_comp.dart';
 import 'package:webclient/components/account/change_password_comp.dart';
 
-import 'package:webclient/components/view_contest/fantasy_team_comp.dart';
-import 'package:webclient/components/view_contest/users_list_comp.dart';
+import 'package:webclient/components/my_contests_comp.dart';
 import 'package:webclient/components/view_contest/view_contest_entry_comp.dart';
 import 'package:webclient/components/view_contest/view_contest_comp.dart';
+import 'package:webclient/components/view_contest/fantasy_team_comp.dart';
+import 'package:webclient/components/view_contest/users_list_comp.dart';
 
 import 'package:webclient/components/enter_contest/enter_contest_comp.dart';
 import 'package:webclient/components/enter_contest/lineup_selector_comp.dart';
@@ -76,7 +76,6 @@ class WebClientApp extends Module {
     if (HostServer.isProd) {
       TemplateCache cache = new TemplateCache();
       primeTemplateCache(cache);
-
       bind(TemplateCache, toValue: cache);
     }
 
@@ -84,6 +83,7 @@ class WebClientApp extends Module {
     bind(ServerService, toImplementation: DailySoccerServer);
 
     bind(ScreenDetectorService);
+
     bind(LoadingService);
     bind(RefreshTimersService);
     bind(DateTimeService);
@@ -91,6 +91,7 @@ class WebClientApp extends Module {
     bind(FlashMessagesService);
 
     bind(ActiveContestsService);
+
     bind(MyContestsService);
     bind(SoccerPlayerService);
     bind(ScoringRulesService);
@@ -109,8 +110,8 @@ class WebClientApp extends Module {
     bind(LoginComp);
     bind(JoinComp);
 
-    bind(ContestsListComp);
     bind(LobbyComp);
+    bind(ContestsListComp);
     bind(PromosComp);
     bind(PaginatorComp);
     bind(ContestFiltersComp);

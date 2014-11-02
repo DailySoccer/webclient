82d60836b51836be7b881857b9b2cf01f4bcc9ba
vmendi <vmendi@gmail.com>
2014-11-01 23:36:40 -0700

    build for deploy se basa en el otro script.

--- build_for_deploy.sh
+++ build_for_deploy.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
-mode="release"
-
-if [[ $1 != "" ]]
-    then
-        mode=$1
-fi
-
-# Cambios al index.html
-sed -i".bak" '/<!--/{ N; N; s/.*\n\(.*main.dart.js.*\)\n.*-->.*/\1/; }' web/index.html
-sed -i".bak" '/src="main.dart"/d' web/index.html
-sed -i".bak" '/src="packages\/browser\/dart.js"/d' web/index.html
-rm web/index.html.bak
-
-# Compilamos nuestros less a css
-./compile_less.sh
-
-# mode puede ser debug|release
-echo "Client compilation mode is: $mode"
-dart cache_gen.dart
-pub build --mode=$mode
+source build.sh $mode
 rsync -r  -v --copy-unsafe-links build/web/. ../backend/public/
 
 # Revertimos los cambios hechos a distintos ficheros

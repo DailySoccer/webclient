5736540645b913bfa420dd1f18fb4eb6f012ba1d
Fede Mon <gnu.fede@gmail.com>
2014-11-20 10:51:56 +0100

    Cambiado botón de FB al estándar

--- lib/components/account/login_comp.dart
+++ lib/components/account/login_comp.dart
@@ -8,7 +8,7 @@ import 'package:webclient/utils/string_utils.dart';
 import 'package:webclient/services/loading_service.dart';
 import 'package:webclient/models/connection_error.dart';
 import 'package:webclient/utils/js_utils.dart';
-import 'dart:js';
+import 'dart:js' as js;
 import 'package:logging/logging.dart';
 
 @Component(
@@ -24,15 +24,18 @@ class LoginComp implements ShadowRootAware {
 
   bool get enabledSubmit => StringUtils.isValidEmail(email) && password.isNotEmpty && _enabledSubmit;
 
-  LoginComp(this._router, this._profileManager, this.loadingService, this._rootElement);
+  LoginComp(this._router, this._profileManager, this.loadingService, this._rootElement) {
+    js.context['jsLoginFB'] = loginFB;
+  }
 
   @override void onShadowRoot(emulatedRoot) {
     _errSection = _rootElement.querySelector("#mailPassError");
     _errSection.parent.parent.style.display = 'none';
   }
 
+
   void loginFB() {
-    JsUtils.runJavascript(null, "getLoginStatus", [(JsObject statusResponse) {
+    JsUtils.runJavascript(null, "getLoginStatus", [(js.JsObject statusResponse) {
       if (statusResponse["status"]=="connected") {
         loginCallback(statusResponse);
       }
@@ -40,7 +43,7 @@ class LoginComp implements ShadowRootAware {
         // El usuario no ha autorizado el uso de su facebook.
       }
       else {
-        JsUtils.runJavascript(null, "facebookLogin", [(JsObject loginResponse) {
+        JsUtils.runJavascript(null, "facebookLogin", [(js.JsObject loginResponse) {
           print("facebook login: $loginResponse");
           if (loginResponse["status"]=="connected") {
             loginCallback(loginResponse);
@@ -57,6 +60,9 @@ class LoginComp implements ShadowRootAware {
     }, true],
     false, "FB");
   }
+
+
+
 
   void loginCallback(loginResponse) {
     _profileManager.facebookLogin(loginResponse["authResponse"]["accessToken"])

f12fdf7839d44698dcc98afa17e6d17c9a8b61db
Fede Mon <gnu.fede@gmail.com>
2014-12-29 17:18:58 +0100

    Testing transformer

--- lib/transformer_test.dart
+++ lib/transformer_test.dart
@@ -1,13 +1,18 @@
+library webclient;
+
 import 'package:barback/barback.dart';
+import 'dart:async';
 
 class TransformerTest extends Transformer {
-  final BarbackSettings _settings;
 
-  TransformerTest.asPlugin(this._settings);
+  TransformerTest.asPlugin();
 
   Future apply(Transform transform) {
-    // Skip the transform in debug mode.
-    if (_settings.mode.name == 'debug') return;
+    return transform.primaryInput.readAsString().then((content) {
+      String newContent = content.toString().replaceFirst("<script src=\"main.dart\" id=\"mainDart\"></script>\n",
+        "<script src=\"main.dart.js\" id=\"mainDart\"></script>\n");
+      transform.addOutput(new Asset.fromString(transform.primaryInput.id, newContent));
+    });
+    }
 
-  }
 }

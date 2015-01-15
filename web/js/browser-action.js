956662c165f5aa7132e14277209e11d065dfe23a
Fede Mon <gnu.fede@gmail.com>
2015-01-15 15:59:43 +0100

    Browser version comparison fixed

--- web/js/browser-action.js
+++ web/js/browser-action.js
@@ -5,23 +5,23 @@ function isValidBrowser(userAgentDetected) {
 
     switch (userAgentDetected.browser.name) {
         case "IE":
-            valid = userAgentDetected.browser.version >= "9";
+            valid = parseInt(userAgentDetected.browser.version) >= 9;
             break;
 
         case "Firefox":
-            valid = userAgentDetected.browser.version >= "16";
+            valid = parseInt(userAgentDetected.browser.version) >= 16;
             break;
 
         case "Chrome":
-            valid = userAgentDetected.browser.version >= "27";
+            valid = parseInt(userAgentDetected.browser.version) >= 27;
             break;
 
         case "Safari":
-            valid = userAgentDetected.browser.version >= "7";
+            valid = parseInt(userAgentDetected.browser.version) >= 7;
             break;
 
         case "Opera":
-            valid = userAgentDetected.browser.version >= "12";
+            valid = parseInt(userAgentDetected.browser.version) >= 12;
             break;
     }
 

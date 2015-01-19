927410b16a7b82b99e52f0306f7e520a06dc1dfe
SantiagoRevelo <sreveloc@gmail.com>
2015-01-19 11:18:53 +0100

    Mejora de creación del HTML

--- lib/components/landing_page_1_slide_comp.dart
+++ lib/components/landing_page_1_slide_comp.dart
@@ -27,75 +27,13 @@ class LandingPage1SlideComp implements ShadowRootAware, DetachAware {
   }
 
   void _composeDesktopHtml() {
-  String theDesktopHTML =
-  '''
-    <div class="screen-pattern"></div> 
-    <div id="landingPageRoot">
-      <div id="desktopContent">    
-        <div class="main-title-wrapper">
-    
-          <div class="main-title">LIGAS FANTÁSTICAS SEMANALES</div>
-          <div class="main-sub-title">
-            Juega y gana cuando quieras, sin esperar al final de la temporada.
-          </div>
-          <div class="button-wrap">
-            <button type="button" class="button-play" buttonOnclick="join" id="playButton1">ÚNETE Y JUEGA GRATIS</button>
-          </div>
-          <div class="text-wrapper">
-    
-            <div class="module-column">
-              <p class="icono-text">Compite en tantos torneos como quieras de Liga, Premier y Champions</p>
-            </div>
-    
-            <div class="module-column">
-              <p class="icono-text">Crea tu equipo en segundos desde cualquier dispositivo</p>
-            </div>
-    
-            <div class="module-column">
-              <p class="icono-text">Podrás ganar dinero en efectivo con pagos inmediatos</p>
-            </div>
-    
-          </div>
-          <div class="modules-wrapper">
-            <div class="module-column">
-              <img src="images/iconsLeagues.png">
-            </div>
-            <div class="module-column">
-              <img src="images/iconsDevices.png">
-            </div>
-            <div class="module-column">
-              <img src="images/iconsPayment.png">
-            </div>
-          </div>
-    
-        </div>
-        <div class="beta-label"><img src="images/beta.png"/></div>
-      </div>
-    </div>
-    ''';
-   _createHTML(theDesktopHTML);
+    String html = _commonHTML.replaceAll('@HTMLContent', _theDesktopHTML);
+    _createHTML(html);
   }
 
   void _ComposeMobileHtml() {
-    String theMobileHTML =
-    '''
-      <div class="screen-pattern"></div>
-      <div id="landingPageRoot">
-        <div id="mobileContent">
-          <div class="content">
-            <p class="main-title-mobile">LIGAS FANTÁSTICAS <br> SEMANALES</p>
-            <p class="title-sup-text-mobile">COMPITE EN TANTOS TORNEOS COMO QUIERAS</p>
-            <p class="title-sup-text-mobile">CREA TU EQUIPO EN SEGUNDOS</p>
-            <p class="title-sup-text-mobile">Y GANA DINERO</p>
-            <div class="button-wrap">
-              <button type="button" class="button-play-mobile" buttonOnclick="join" id="playButtonMobile">ÚNETE Y JUEGA GRATIS</button>
-            </div>
-          </div>
-          <div class="beta-label"><img src="images/beta.png"/></div>
-        </div>
-      </div>
-    ''';
-    _createHTML(theMobileHTML);
+    String html = _commonHTML.replaceAll('@HTMLContent', _theMobileHTML);
+    _createHTML(html);
   }
 
   void _createHTML(String theHTML) {
@@ -139,6 +77,73 @@ class LandingPage1SlideComp implements ShadowRootAware, DetachAware {
     scrDet.scrollTo('#mainWrapper', offset: 0, duration:  0, smooth: false, ignoreInDesktop: false);
   }
 
+  String _commonHTML =
+    '''
+      <div class="screen-pattern"></div>
+      <div id="landingPageRoot">
+        @HTMLContent
+        <div class="beta-label"><img src="images/beta.png"/></div>
+      </div>
+    ''';
+
+  String _theDesktopHTML =
+    '''
+      <div id="desktopContent">    
+        <div class="main-title-wrapper">
+    
+          <div class="main-title">LIGAS FANTÁSTICAS SEMANALES</div>
+          <div class="main-sub-title">
+            Juega y gana cuando quieras, sin esperar al final de la temporada.
+          </div>
+          <div class="button-wrap">
+            <button type="button" class="button-play" buttonOnclick="join" id="playButton1">ÚNETE Y JUEGA GRATIS</button>
+          </div>
+          <div class="text-wrapper">
+    
+            <div class="module-column">
+              <p class="icono-text">Compite en tantos torneos como quieras de Liga, Premier y Champions</p>
+            </div>
+    
+            <div class="module-column">
+              <p class="icono-text">Crea tu equipo en segundos desde cualquier dispositivo</p>
+            </div>
+    
+            <div class="module-column">
+              <p class="icono-text">Podrás ganar dinero en efectivo con pagos inmediatos</p>
+            </div>
+    
+          </div>
+          <div class="modules-wrapper">
+            <div class="module-column">
+              <img src="images/iconsLeagues.png">
+            </div>
+            <div class="module-column">
+              <img src="images/iconsDevices.png">
+            </div>
+            <div class="module-column">
+              <img src="images/iconsPayment.png">
+            </div>
+          </div>
+    
+        </div>
+        
+      </div>
+    ''';
+
+  String _theMobileHTML =
+  '''
+        <div id="mobileContent">
+          <div class="content">
+            <p class="main-title-mobile">LIGAS FANTÁSTICAS <br> SEMANALES</p>
+            <p class="title-sup-text-mobile">COMPITE EN TANTOS TORNEOS COMO QUIERAS</p>
+            <p class="title-sup-text-mobile">CREA TU EQUIPO EN SEGUNDOS</p>
+            <p class="title-sup-text-mobile">Y GANA DINERO</p>
+            <div class="button-wrap">
+              <button type="button" class="button-play-mobile" buttonOnclick="join" id="playButtonMobile">ÚNETE Y JUEGA GRATIS</button>
+            </div>
+          </div> 
+        </div>
+    ''';
   var _streamListener;
 
   int _windowHeigtht;

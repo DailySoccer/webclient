8134f3646f354dd3502d0d391fb5e4152c324af2
SantiagoRevelo <sreveloc@gmail.com>
2015-02-04 18:51:07 +0100

    maquetada en XS

--- lib/models/transaction_info.dart
+++ lib/models/transaction_info.dart
@@ -3,6 +3,18 @@ library transaction_info;
 import 'package:webclient/services/datetime_service.dart';
 
 class TransactionInfo {
+
+  Map transactionTypes = {
+    "PRIZE"               : "Premio",
+    "ORDER"               : "Transacción",
+    "ENTER_CONTEST"       : "Participación en torneo",
+    "CANCEL_CONTEST_ENTRY": "Cancelar participación en torneo",
+    "CANCEL_CONTEST"      : "Torneo cancelado",
+    "REFUND"              : "Reembolso",
+    "FREE_MONEY"          : "Esta ronda la paga la casa"
+  };
+
+  String transactionDescription;
   String type;
   double value;
   DateTime createdAt;
@@ -17,7 +29,10 @@ class TransactionInfo {
   TransactionInfo.fromJsonObject(Map jsonMap) {
     type = jsonMap["type"];
     value = double.parse(jsonMap["value"]);
+    transactionDescription = transactionTypes[type];
     transactionID = jsonMap["accountingTranId"];
     createdAt = DateTimeService.fromMillisecondsSinceEpoch(int.parse(jsonMap["createdAt"]));
   }
+
+
 }

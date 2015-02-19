f85b40e7016e91e541bebad72a636c1143376583
Ximo <ximo.m.albors@gmail.com>
2015-02-19 10:53:31 +0100

    fix payment_response: mensajes

--- lib/components/account/payment_response_comp.dart
+++ lib/components/account/payment_response_comp.dart
@@ -13,10 +13,14 @@ class PaymentResponseComp implements ShadowRootAware {
 
   String result;
   String titleText;
+  String descriptionText;
 
   PaymentResponseComp(this._routeProvider, this._router, this._paymentService) {
     result = _routeProvider.route.parameters['result'];
-    titleText = result == 'success'? 'correcto' : 'cancelado';
+    titleText = result == 'success' ? "successful payment" : "cancelled payment";
+    descriptionText = result == 'success'
+        ? "Thank you! Your order has been successfully processed."
+        : "The requested transaction cannot be completed. Please check your payment method and try again.";
   }
 
   @override void onShadowRoot(emulatedRoot) {
@@ -26,7 +30,7 @@ class PaymentResponseComp implements ShadowRootAware {
   void closeModal(Event e) {
     querySelector("#modalRoot").click();
   }
-  
+
   PaymentService _paymentService;
   RouteProvider _routeProvider;
   Router _router;

3045204b2a775c05d8fc6a9a864b5a4060d031a4
korgan00 <korgan00@hotmail.com>
2015-11-12 20:05:36 +0100

    home check user logged

--- lib/components/home_comp.dart
+++ lib/components/home_comp.dart
@@ -3,6 +3,7 @@ library home_comp;
 import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:webclient/services/tutorial_service.dart';
+import 'package:webclient/services/profile_service.dart';
 
 @Component(
   selector: 'home',
@@ -11,23 +12,27 @@ import 'package:webclient/services/tutorial_service.dart';
 )
 class HomeComp  {
 
-  HomeComp(this._router, TutorialService tutorialService);
+  HomeComp(this._router, this._profileService, TutorialService tutorialService);
 
   void onContestsClick() {
     _router.go('lobby', {});
   }
+  
   void onScoutingClick() {
-
+    if (!userIsLogged) return;
   }
   void onCreateContestClick() {
-
+    if (!userIsLogged) return;
   }
   void onHistoryClick() {
+    if (!userIsLogged) return;
     _router.go('my_contests', {'section':'history'});
   }
   void onLiveClick() {
+    if (!userIsLogged) return;
     _router.go('my_contests', {'section':'live'});
   }
+  
   void onBlogClick() {
 
   }
@@ -35,6 +40,9 @@ class HomeComp  {
     _router.go('tutorial_list', {});
   }
 
+  bool get userIsLogged => _profileService.isLoggedIn;
+  
+  ProfileService _profileService;
   Router _router;
 
 }

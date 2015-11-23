3527b0d455480b19561c56b198a678d1b75b16f0
sreveloc <sreveloc@gmail.com>
2015-11-23 19:07:01 +0100

    Elementos de Moneda

--- lib/components/account/shop_comp.dart
+++ lib/components/account/shop_comp.dart
@@ -5,42 +5,61 @@ import 'package:webclient/components/modal_comp.dart';
 import 'package:webclient/services/loading_service.dart';
 import 'package:webclient/utils/string_utils.dart';
 import 'dart:html';
+import 'package:webclient/services/flash_messages_service.dart';
+import 'package:webclient/services/catalog_service.dart';
+import 'package:webclient/models/product.dart';
 
 @Component(
     selector: 'shop-comp',
     templateUrl: 'packages/webclient/components/account/shop_comp.html',
     useShadowDom: false
 )
-class ShopComp implements DetachAware {
+class ShopComp {
 
   LoadingService loadingService;
 
-  List<Map> shops;
+  List<Map> goldProducts;
 
   String getLocalizedText(key) {
     return StringUtils.translate(key, "shop");
   }
+  
 
-  ShopComp(this._router) {
-    shops = [
-        {"name" : "gold",           "image" : "images/shopItemCoin.png",          "description" : getLocalizedText('goldshopdescription')}
-       // ,{"name" : "trainer_points", "image" : "images/shopItemTrainerPoints.png", "description" : getLocalizedText('trainerpointsshopdescription')}
-       ,{"name" : "energy",         "image" : "images/shopItemEnergy.png",        "description" : getLocalizedText('energyshopdescription')}
-    ];
+  ShopComp(this._flashMessage, this._catalogService) {
+    goldProducts = [];
+
+    _catalogService.getCatalog()
+      .then((catalog) {
+        for (Product info in catalog.where((product) => product.gained.isGold)) {
+          Map product = {};
+          product["id"]             = info.id;
+          product["description"]    = getLocalizedText(info.name);
+          product["captionImage"]   = info.imageUrl;
+          product["price"]          = info.price.toStringWithCurrency();
+          product["quantity"]       = info.gained.amount.toInt().toString();
+          product["freeIncrement"]  = info.free.amount.toInt();
+          product["isMostPopular"]  = info.mostPopular;
+          product["purchasable"]    = true;
+          goldProducts.add(product);
+        }
+    });
+  }
+  
+  void buyItem(String id) {
+    _catalogService.buyProduct(id)
+      .then( (_) {
+        if (window.localStorage.containsKey("add_gold_success")) {
+
+          window.location.assign(window.localStorage["add_gold_success"]);
+        }
+        else {
+          Map product = goldProducts.firstWhere((product) => product["id"] == id, orElse: () => {});
+          _flashMessage.addGlobalMessage("Has comprado [${product["description"]}]", 1);
+        }
+    });
   }
 
-  void detach() {
-    window.localStorage.remove("add_funds_success");
-    window.localStorage.remove("add_gold_success");
-    window.localStorage.remove("add_energy_success");
-  }
-
-  void openShop(String name) {
-    if (name != 'trainer_points') {
-      ModalComp.open(_router, "shop." + name, {});
-    }
-  }
-
-  Router _router;
+  FlashMessagesService _flashMessage;
+  CatalogService _catalogService;
 }
 

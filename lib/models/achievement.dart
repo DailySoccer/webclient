668eb77f464634ffb4952e3441664ef40e4f5b77
Ximo <ximo.m.albors@gmail.com>
2015-11-24 15:06:29 +0100

    achievement udpated: name && description

--- lib/models/achievement.dart
+++ lib/models/achievement.dart
@@ -6,8 +6,8 @@ class Achievement {
   static const String PLAYED_VIRTUAL_CONTESTS_LEVEL_2 = "PLAYED_10_VIRTUAL_CONTESTS";
 
   static const String WON_VIRTUAL_CONTESTS_LEVEL_1 = "WON_1_VIRTUAL_CONTEST";
-  static const String WON_VIRTUAL_CONTESTS_LEVEL_5 = "WON_5_VIRTUAL_CONTESTS";
-  static const String WON_VIRTUAL_CONTESTS_LEVEL_10 = "WON_10_VIRTUAL_CONTESTS";
+  static const String WON_VIRTUAL_CONTESTS_LEVEL_2 = "WON_5_VIRTUAL_CONTESTS";
+  static const String WON_VIRTUAL_CONTESTS_LEVEL_3 = "WON_10_VIRTUAL_CONTESTS";
 
   static const String FP_VIRTUAL_CONTEST_LEVEL_1 = "FP_500_VIRTUAL_CONTEST";
   static const String FP_VIRTUAL_CONTEST_LEVEL_2 = "FP_700_VIRTUAL_CONTEST";
@@ -22,8 +22,8 @@ class Achievement {
   static const String PLAYED_OFFICIAL_CONTESTS_LEVEL_2 = "PLAYED_10_OFFICIAL_CONTESTS";
 
   static const String WON_OFFICIAL_CONTESTS_LEVEL_1 = "WON_1_OFFICIAL_CONTEST";
-  static const String WON_OFFICIAL_CONTESTS_LEVEL_5 = "WON_5_OFFICIAL_CONTESTS";
-  static const String WON_OFFICIAL_CONTESTS_LEVEL_10 = "WON_10_OFFICIAL_CONTESTS";
+  static const String WON_OFFICIAL_CONTESTS_LEVEL_2 = "WON_5_OFFICIAL_CONTESTS";
+  static const String WON_OFFICIAL_CONTESTS_LEVEL_3 = "WON_10_OFFICIAL_CONTESTS";
 
   static const String FP_OFFICIAL_CONTEST_LEVEL_1 = "FP_500_OFFICIAL_CONTEST";
   static const String FP_OFFICIAL_CONTEST_LEVEL_2 = "FP_700_OFFICIAL_CONTEST";
@@ -93,79 +93,191 @@ class Achievement {
 
   static List<Map> AVAILABLES = [
     {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "asd",
-      "description": "qweqweqweqwe",
+      "id": WON_VIRTUAL_CONTESTS_LEVEL_1,
+      "name": "El Principiante",
+      "description": "Ganar el primer torneo virtual",
       "image": 'IconManagerMister.png',
       "style": 'Training'
     },
     {
       "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "xcv",
-      "description": "qweqweqweqwe",
+      "name": "Superentreno",
+      "description": "Jugar 10 torneos virtuales",
       "image": '',
       "style": 'Training'
     },
     {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "ser",
-      "description": "eeeeeeee",
+      "id": WON_VIRTUAL_CONTESTS_LEVEL_2,
+      "name": "El Hacha",
+      "description": "Ganar 10 torneos virtuales",
       "image": '',
       "style": 'Oficial'
     },
     {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "sds",
-      "description": "ddddddd",
+      "id": FP_VIRTUAL_CONTEST_LEVEL_1,
+      "name": "El Resultón",
+      "description": "Puntuar más de 1000 FP con un equipo en un torneo virtual",
       "image": '',
       "style": 'Oficial'
     },
     {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "asd",
-      "description": "cccccccc",
+      "id": DIFF_FP_VIRTUAL_CONTEST_LEVEL_1,
+      "name": "El Míster",
+      "description": "Ganar un torneo virtual por más de 200 FP de diferencia",
       "image": '',
       "style": 'Training'
     },
     {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "asd",
-      "description": "qweqweqweqwe",
+      "id": WON_OFFICIAL_CONTESTS_LEVEL_1,
+      "name": "El Debutante",
+      "description": "Ganar el primer torneo oficial",
       "image": 'IconManagerPrincipiante.png',
       "style": 'Training'
     },
     {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "asd",
-      "description": "qweqweqweqwe",
+      "id": PLAYED_OFFICIAL_CONTESTS_LEVEL_1,
+      "name": "La Reválida",
+      "description": "Jugar 10 torneos oficiales",
       "image": 'IconManagerMister.png',
       "style": 'Training'
     },
     {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "asd",
-      "description": "qweqweqweqwe",
+      "id": WON_OFFICIAL_CONTESTS_LEVEL_2,
+      "name": "La Consolidación",
+      "description": "Ganar N torneos oficiales",
       "image": 'IconManagerMister.png',
       "style": 'Oficial'
     },
     {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "asd",
-      "description": "qweqweqweqwe",
-      "image": 'IconManagerPrincipiante.png',
-      "style": 'Player'
-    },
-    {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "asd",
-      "description": "qweqweqweqwe",
+      "id": FP_OFFICIAL_CONTEST_LEVEL_1,
+      "name": "La Gloria",
+      "description": "Puntuar más de 1000 FP con un equipo en un torneo oficial",
       "image": 'IconManagerPrincipiante.png',
       "style": 'Player'
     },
     {
-      "id": PLAYED_VIRTUAL_CONTESTS_LEVEL_1,
-      "name": "asd",
-      "description": "qweqweqweqwe",
+      "id": DIFF_FP_OFFICIAL_CONTEST_LEVEL_1,
+      "name": "Máster",
+      "description": "Ganar un torneo oficial por más de 200 FP de diferencia",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'Player'
+    },
+    {
+      "id": SOCCER_PLAYER_WON_FP_LEVEL_1,
+      "name": "El Crack",
+      "description": "Un jugador consigue más de 200 FP en un torneo oficial",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": GOALKEEPER_SAVES_SHOTS_LEVEL_1,
+      "name": "El Santo",
+      "description": "Portero hace más de 20 paradas",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'Training'
+    },
+    {
+      "id": GOALKEEPER_0_GOAL_RECEIVED,
+      "name": "El Autobús",
+      "description": "Portero recibe 0 goles",
+      "image": 'IconManagerMister.png',
+      "style": 'Training'
+    },
+    {
+      "id": GOALKEEPER_RED_CARD,
+      "name": "El Leñero",
+      "description": "Portero expulsado",
+      "image": 'IconManagerMister.png',
+      "style": 'Oficial'
+    },
+    {
+      "id": GOALKEEPER_GOAL_SCORED,
+      "name": "Al Ataque",
+      "description": "Portero hace gol",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'Player'
+    },
+    {
+      "id": DEFENDER_INTERCEPTIONS_LEVEL_1,
+      "name": "El Muro",
+      "description": "Defensa hace más de 30 intercepciones",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'Player'
+    },
+    {
+      "id": MIDDLE_PASS_SUCCESSFUL_LEVEL_1,
+      "name": "El Jugón",
+      "description": "Centrocampista hace más de 70 pases",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": FORWARD_GOAL_LEVEL_1,
+      "name": "El Pistolero",
+      "description": "Delantero hace más de 4 goles",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": ALL_SOCCER_PLAYERS_WITH_FP,
+      "name": "El Equipo",
+      "description": "Todos los jugadores puntúan",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": TRUE_SKILL_LEVEL_1,
+      "name": "Nivel de Skill 1",
+      "description": "Nivel de Skill 1",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": TRUE_SKILL_LEVEL_2,
+      "name": "Nivel de Skill 2",
+      "description": "Nivel de Skill 2",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": TRUE_SKILL_LEVEL_3,
+      "name": "Nivel de Skill 3",
+      "description": "Nivel de Skill 3",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": TRUE_SKILL_LEVEL_4,
+      "name": "Nivel de Skill 4",
+      "description": "Nivel de Skill 4",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": TRUE_SKILL_LEVEL_5,
+      "name": "Nivel de Skill 5",
+      "description": "Nivel de Skill 5",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": MANAGER_LEVEL_3,
+      "name": "Nivel de Manager 3",
+      "description": "Nivel de Manager 3",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": MANAGER_LEVEL_4,
+      "name": "Nivel de Manager 4",
+      "description": "Nivel de Manager 4",
+      "image": 'IconManagerPrincipiante.png',
+      "style": 'SkillLevel'
+    },
+    {
+      "id": MANAGER_LEVEL_5,
+      "name": "Nivel de Manager 5",
+      "description": "Nivel de Manager 5",
       "image": 'IconManagerPrincipiante.png',
       "style": 'SkillLevel'
     }

5bf8ae859a0ea03c4287bc4d1090c5afb42f9b86
korgan00 <korgan00@hotmail.com>
2015-11-24 13:13:31 +0100

    achivements color definitions

--- lib/components/achivements_comp.dart
+++ lib/components/achivements_comp.dart
@@ -19,5 +19,39 @@ class AchivementsComp {
 */
   AchivementsComp (/*this.loadingService, this.profileService*/) {
   }
+  
+  List<Achivement> achivementList = [
+      new Achivement(name: "asd", description: "qweqweqweqwe", image: 'IconManagerMister.png', style: 'Training', earned: true),
+      new Achivement(name: "xcv", description: "qweqweqweqwe", image: '', style: 'Training', earned: true),
+      new Achivement(name: "ser", description: "eeeeeeee", image: '', style: 'Oficial', earned: false),
+      new Achivement(name: "sds", description: "ddddddd", image: '', style: 'Oficial', earned: true),
+      new Achivement(name: "asd", description: "cccccccc", image: '', style: 'Training', earned: true),
+      new Achivement(name: "asd", description: "qweqweqweqwe", image: 'IconManagerPrincipiante.png', style: 'Training', earned: true),
+      new Achivement(name: "asd", description: "qweqweqweqwe", image: 'IconManagerMister.png', style: 'Oficial', earned: true),
+      new Achivement(name: "asd", description: "qweqweqweqwe", image: 'IconManagerMister.png', style: 'ManagerLevel', earned: true),
+      new Achivement(name: "asd", description: "qweqweqweqwe", image: 'IconManagerPrincipiante.png', style: 'Player', earned: true),
+      new Achivement(name: "asd", description: "qweqweqweqwe", image: 'IconManagerPrincipiante.png', style: 'SkillLevel', earned: true)
+    ];
 
+}
+
+class Achivement {
+
+  String name;
+  String description;
+  String image;
+  String style;
+  bool earned;
+
+  static const BASIC_STYLE = "basic";
+  static const ORANGE_STYLE = "orange";
+  
+  Achivement({String name: "", String description: "", String image: "", String style: BASIC_STYLE, bool earned: false}) {
+    this.name = name;
+    this.description = description;
+    this.image = image;
+    this.style = style;
+    this.earned = earned;
+  }
+  
 }

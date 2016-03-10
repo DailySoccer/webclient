aba0cbe53cd21ba584b1bde800684ad8d5922c5e
korgan00 <korgan00@hotmail.com>
2016-03-10 13:11:33 +0100

    facebook better feel and signup fix

--- lib/components/account/join_comp.dart
+++ lib/components/account/join_comp.dart
@@ -297,6 +297,10 @@ class JoinComp implements ShadowRootAware {
     return errorMap[keyError];
   }
 
+  void makeFacebookSignUp() {
+    _fbLogin.loginFB();
+  }
+  
   bool get isFacebookConnected => _fbLogin.isConnected;
 
   FBLogin _fbLogin;

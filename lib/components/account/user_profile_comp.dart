7f0cda52cec02e44c180d66553b4bc0aee377ea9
korgan00 <korgan00@hotmail.com>
2016-04-12 18:24:57 +0200

    some of the states of login

--- lib/components/account/user_profile_comp.dart
+++ lib/components/account/user_profile_comp.dart
@@ -10,6 +10,12 @@ import 'package:webclient/utils/game_metrics.dart';
 import 'package:webclient/components/modal_comp.dart';
 import 'package:logging/logging.dart';
 import 'package:webclient/services/server_error.dart';
+import 'dart:async';
+import 'package:webclient/services/contests_service.dart';
+import 'package:webclient/utils/html_utils.dart';
+import 'package:webclient/utils/js_utils.dart';
+import 'package:webclient/components/backdrop_comp.dart';
+import 'dart:html';
 
 @Component(
     selector: 'user-profile',
@@ -22,7 +28,7 @@ class UserProfileComp {
 
   bool isEditingProfile = false;
 
-  dynamic get userData => _profileManager.user;
+  dynamic get userData => _profileService.user;
 
   Map playerSkillInfo = {'position':'_', 'id':'', 'name': '', 'points': ' '};
   Map playerMoneyInfo = {'position':'_', 'id':'', 'name': '', 'points': '\$ '};
@@ -31,7 +37,7 @@ class UserProfileComp {
     return StringUtils.translate(key, group);
   }
 
-  UserProfileComp(this._router, this._profileManager, this.loadingService, LeaderboardService leaderboardService) {
+  UserProfileComp(this._router, this._profileService, this.loadingService, LeaderboardService leaderboardService) {
     loadingService.isLoading = true;
     leaderboardService.getUsers()
           .then((List<User> users) {
@@ -98,7 +104,7 @@ class UserProfileComp {
     _router.go('shop', {});
   }
   void goLeaderboard() { 
-    _router.go('leaderboard', {'section': 'points', 'userId': _profileManager.user.userId});
+    _router.go('leaderboard', {'section': 'points', 'userId': _profileService.user.userId});
   }
 
   void bindWithFutbolCuatroJoin() {
@@ -115,35 +121,165 @@ class UserProfileComp {
       String nickName = params["nickName"];
       String password = params["password"];
       Function onError = params["onError"];
-      
-      //_profileService.signup(firstName, lastName, email, nickName, password)
-      
+
+      getAccount(email, password, (ServerError error) {
+        if (error.responseError == "not_exists") {
+          loadingService.isLoading = true;
+          _profileService.bindUUID(firstName, lastName, email, nickName, password)
+              .then(([_]) { loadingService.isLoading = false; })
+              .catchError((ServerError error) {
+                loadingService.isLoading = false;
+                Logger.root.severe(error);
+              }, test: (error) => error is ServerError);
+        } else {
+          onError(error);
+        }
+      });
       
     } else if (params["action"] == "login") {
       String email = params["email"];
       String password = params["password"];
       Function onError = params["onError"];
 
-      loadingService.isLoading = true;
-      _profileManager.getAccount(email, password).then((accountInfo) {
-          loadingService.isLoading = false;
-          
-          // Select the right account
-          
-          
-          ModalComp.deleteCallback();
-          ModalComp.close();
-          
-        }).catchError((ServerError error) {
-          loadingService.isLoading = false;
-          onError(error);
-        }, test: (error) => error is ServerError);
+      getAccount(email, password, onError);
       
     } else {
       Logger.root.severe("Unknown BindWithFutbolCuatro process on user profile");
     }
   }
+  
+  Future getAccount(String email, String password, void onError(ServerError)) {
+    loadingService.isLoading = true;
+    _profileService.getAccount(email, password).then((Map accountInfo) {
+          loadingService.isLoading = false;
 
-  ProfileService _profileManager;
+          ModalComp.deleteCallback();
+          ModalComp.close();
+          
+          // Select the right account
+          selectAccount(accountInfo);
+          
+        }).catchError((ServerError error) {
+            loadingService.isLoading = false;
+            onError(error);
+        }, test: (error) => error is ServerError);
+  }
+  
+  void selectAccount(Map cloudAccountInfo) {
+    Map deviceAccountInfo = {};
+    _contestsService.countMyContests().then((Map jsonData) {
+      loadingService.isLoading = false;
+      
+      num numVirtualHistoryContests = jsonData.containsKey("numVirtualHistory") ? jsonData["numVirtualHistory"] : 0;
+      num numRealHistoryContests    = jsonData.containsKey("numRealHistory") ? jsonData["numRealHistory"] : 0;
+      num numLiveContests           = jsonData.containsKey("numLive") ? jsonData["numLive"] : 0;
+      num numUpcomingContests       = jsonData.containsKey("numWaiting") ? jsonData["numWaiting"] : 0;
+      
+      deviceAccountInfo["name"] = _profileService.user.nickName;
+      deviceAccountInfo["balance"] = _profileService.user.balance;
+      deviceAccountInfo["managerLevel"] = _profileService.user.managerLevel;
+      deviceAccountInfo["historyCount"] = numVirtualHistoryContests + numRealHistoryContests;
+      deviceAccountInfo["playingCount"] = numLiveContests + numUpcomingContests;
+      
+
+      ModalComp.open(_router, "user_profile.selectAccount", {}, bindWithFutbolCuatroCallback);
+
+      modalSelectAccount(deviceAccountInfo, cloudAccountInfo);
+        
+
+    });
+  }
+  
+
+  Future<bool> modalSelectAccount(deviceAccount, cloudAccount) {
+  
+    Element modalWindow = querySelector("#modalWindow");
+  
+    void onClose(dynamic sender) {
+      modalWindow.children.remove(modalWindow.querySelector('#alertRoot'));
+    }
+  
+    void onButtonClick(dynamic sender) {
+      JsUtils.runJavascript('#alertRoot', 'modal', "hide");
+      BackdropComp.instance.hide();
+    }
+    
+    modalWindow.setInnerHtml(''' 
+              <div id="alertRoot" class="modal container modal-select-acount" tabindex="-1" role="dialog" style="display: block;">
+                <div class="modal-dialog modal-lg">
+                  <div class="modal-content"> 
+    
+                    <div class="alert-content">      
+                      <div id="alertBox" class="main-box">
+                        <div class="panel">
+                          <!-- Content Message and Buttons-->
+                          <div class="panel-body" >
+                            <!-- Alert Text -->
+                            <div class="form-description" id="modalContentWrapper">
+                              <button class="ok-button" eventCallback="onCloudAccount">Cloud</button>
+                              <button class="ok-button" eventCallback="onDeviceAccount">Device</button>
+                            </div>            
+                            <!-- Alert Buttons -->
+                            <div class="input-group user-form-field">
+                              <div class="new-row">                  
+                                <div class="autocentered-buttons-wrapper">
+                                  <div class="button-box"><button class="cancel-button" eventCallback="onCancel">Cancel</button></div> 
+                                  <div class="button-box"><button class="ok-button" eventCallback="onConfirm">Ok</button><div>
+                                </div>
+                              </div>
+                            </div>            
+                          </div>
+                        </div>
+                      </div>        
+                    </div>
+    
+                  </div>
+                </div>
+              </div>
+      ''', treeSanitizer: NULL_TREE_SANITIZER);
+
+    // Aqui hago el setup de los botones. (que tiene que hacer cada botón al ser clickado... ver: main_menu_slide_comp).
+    modalWindow.querySelectorAll("[eventCallback]").onClick.listen((sender) {
+      String eventCallback = sender.currentTarget.attributes["eventCallback"];
+      switch (eventCallback) {
+            case "onCancel":
+              print("cancel");
+              break;
+            case "onConfirm":
+              print("confirm");
+              break;
+            case "onCloudAccount":
+              print("cloud");
+              break;
+            case "onDeviceAccount":
+              print("device");
+              break;
+          }
+    });
+  
+    JsUtils.runJavascript('#alertRoot', 'modal', null);
+    JsUtils.runJavascript('#alertRoot', 'on', {'hidden.bs.modal': (sender) { onClose(sender); } });
+    BackdropComp.instance.show();
+  }
+/*
+  String getNotEnoughGoldContent(Money goldNeeded) {
+    return '''
+    <div class="content-wrapper">
+      <img class="main-image" src="images/iconNoGold.png">
+      <span class="not-enough-resources-count">${goldNeeded}</span>
+      <p class="content-text">
+        <strong>${getLocalizedText("alert-no-gold-message")}</strong>
+        <br>
+        ${getLocalizedText('alert-user-gold-message', substitutions: {'MONEY': _profileService.user.goldBalance})}
+        <img src="images/icon-coin-xs.png">
+      </p>
+    </div>
+    ''';
+  }
+*/
+  
+
+  ProfileService _profileService;
+  ContestsService _contestsService;
   Router _router;
 }

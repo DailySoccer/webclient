0c6e6e30ee7d67d79447197328902d44682623b7
sreveloc <sreveloc@gmail.com>
2016-08-30 13:13:23 +0200

    quitado mixpanel

--- web/js/mixpanel_init_unsupported.js
+++
@@ -1,7 +0,0 @@
-    var mixpanelCode = (window.location.hostname.indexOf("epiceleven") > 0)?
-                        "a1889b53bda6b6348f60a570f658c157":
-                        "f627312247ce937f807ce4b9d786314b";
-
-    mixpanel.init(mixpanelCode, {api_host: "https://api.mixpanel.com"});
-    mixpanel.track("Unsupported Browser");
-    console.log("Unsupported Browser");

7a3f4611ac2693eb3ac0566b04bf8908c0730cbd
korgan00 <korgan00@hotmail.com>
2016-11-14 10:36:02 +0100

    dailyReward comp created

---
+++ lib/components/daily_rewards_comp.dart
@@ -0,0 +1,22 @@
+library daily_rewards_comp;
+
+import 'package:angular/angular.dart';
+import 'package:webclient/services/screen_detector_service.dart';
+import 'package:webclient/services/promos_service.dart';
+import 'package:webclient/utils/game_metrics.dart';
+import 'dart:html';
+import 'package:webclient/services/refresh_timers_service.dart';
+
+@Component(
+    selector: 'daily-rewards',
+    templateUrl: 'packages/webclient/components/promos_comp.html',
+    useShadowDom: false
+)
+class DailyRewardsComp {
+
+  DailyRewardsComp() {
+    
+  }
+
+  
+}

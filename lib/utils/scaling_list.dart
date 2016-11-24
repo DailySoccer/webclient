328aed094138ab795df62329383c3a3d5630e0f5
Ximo <ximo.m.albors@gmail.com>
2016-11-25 00:53:05 +0100

    scaling_list: evitar las peticiones simultáneas de _scaleList([_])

--- lib/utils/scaling_list.dart
+++ lib/utils/scaling_list.dart
@@ -4,6 +4,7 @@ import 'package:angular/core/annotation.dart';
 import 'dart:html';
 import 'dart:math';
 import 'dart:collection';
+import 'package:logging/logging.dart';
 // import 'package:logging/logging.dart';
 
 class ScalingList<T> {
@@ -21,11 +22,12 @@ class ScalingList<T> {
     _processList();
   }
   List<T> get elements => _currentList;
-  bool get isFullList => _insertList.length == 0;
+  bool get isFullList => _insertList.isEmpty;
   
   // TODO Cambiar los parámetros opcionales por "named"
   ScalingList(this.initialAmount, [bool eqComparer(T a, T b), bool sorting]) {
     _insertList = new Queue<T>();
+    _referenceList = new Queue<T>();
     _fullList = [];
     _currentList = [];
     if(eqComparer != null) _equalsComparer = eqComparer;
@@ -45,12 +47,12 @@ class ScalingList<T> {
     } else {
       // Si _currentList no está vacía eliminamos todos los que no esten en full list.
       _currentList.removeWhere((t1) => !_fullList.any((t2) => _equalsComparer(t1, t2)));
+
       // Actualizar las referencias (dado que la información de los mismos puede haber cambiado)
-      // _currentList = _currentList.map( (t1) => _fullList.firstWhere((t2) => _equalsComparer(t1, t2), orElse: () => t1) ).toList();
-      for (int i=0; i<_currentList.length; i++) {
-        var el = _currentList[i];
-        _currentList[i] = _fullList.firstWhere((el2) => _equalsComparer(el, el2), orElse: () => el);
-      }
+      // Añadimos todas las que no estén actualmente
+      _referenceList.addAll(_fullList.where((t1) => !_referenceList.any((t2) => _equalsComparer(t1, t2))));
+      window.animationFrame.then(_runUpdateReferences);
+      
       // y añadimos a la insert list los elementos que no están y la posicion en la que hay que insertarlos.
       _insertList.addAll(_fullList.where((t1) => !_currentList.any((t2) => _equalsComparer(t1, t2))));
     }
@@ -83,11 +85,38 @@ class ScalingList<T> {
     }*/
     _setInitialAmount();
     _continueScaling = true;
-    window.animationFrame.then(_scaleList);
+    
+    window.animationFrame.then(_runScaleList);
   }
 
+  int _numScaleList = 0;
+  void _runScaleList([_]) { if (_numScaleList <= 0) { ++_numScaleList; _scaleList(); } }
+  void _endScaleList() { --_numScaleList; }
+
+  int _numUpdateReferences = 0;
+  void _runUpdateReferences([_]) { if (_numUpdateReferences <= 0) { ++_numUpdateReferences; _updateReferences(); } }
+  void _endUpdateReferences() { --_numUpdateReferences; }
+ 
+  void _updateReferences([_]) {
+    if (_referenceList.isNotEmpty) {
+      var el = _referenceList.removeFirst();
+      
+      for (int i=0; i<_currentList.length && i<10; i++) {
+        if (_equalsComparer(el, _currentList[i])) {
+          _currentList[i] = el;
+          break;
+        }
+      }
+      
+      window.animationFrame.then(_updateReferences);
+    }
+    else {
+      _endUpdateReferences();
+    }
+  }
+  
   void _scaleList([_]) {
-    if (_insertList.length > 0 && _continueScaling) {
+    if (_insertList.isNotEmpty && _continueScaling) {
       _currentList.add(_insertList.removeFirst());
       
       if (_sorting) {
@@ -97,17 +126,22 @@ class ScalingList<T> {
       window.animationFrame.then(_scaleList);
       // Logger.root.info("_scaleList: ${_currentList.length} - ${new DateTime.now()}");
     }
+    else {
+      _endScaleList();
+    }
   }
     
   void stopScaling() {
     _continueScaling = false;
   }
+  
   void resumeScaling() {
     if (!_continueScaling) {
       _continueScaling = true;
-      _scaleList();
+      _runScaleList();
     }
   }
+  
   void redraw() {
     if (_currentList != null) {
       // TODO Habrá que buscar un método alternativo de "redraw" cuando la lista no se quiera ordenar
@@ -119,6 +153,7 @@ class ScalingList<T> {
   
   List<T> _fullList;
   Queue<T> _insertList;
+  Queue<T> _referenceList;
   List<T> _currentList;
   bool _continueScaling;
   
